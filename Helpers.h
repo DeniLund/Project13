f70fc6ccf292acf68cb0692ca5495f6f0765d9ff
Denis Lund <vasilenko9999@gmail.com>
2021-12-07 13:51:07 +0300

    Добавьте файлы проекта.

---
+++ Helpers.h
@@ -0,0 +1,6 @@
+
+int sum(int a, int b)
+{
+	return (a + b) * (a + b);
+		// a * a + 2 * a * b + b * b;
+}
---
+++ Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	int result = sum(24, 16);
+	std::cout << "Result - " << result << std::endl;
+}
